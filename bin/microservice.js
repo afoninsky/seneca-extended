ac3b242a98a2382cdc739641a048b3d75d3f88d7
Andrey <vkfont@gmail.com>
2016-09-28 17:02:50 +0300

    current progress

--- bin/microservice.js
+++ bin/microservice.js
@@ -2,17 +2,18 @@
 
 if (module.parent) { throw new Error('Im not a module, please launch me directly. Thank you') }
 
-const config = require('config')
-const seneca = require('../src')
-const { requireDirectory } = require('../src/utils')
-
-const plugins = requireDirectory(`${process.cwd()}/src`)
-const services = process.argv.length > 2 ? process.argv.slice(2) : Object.keys(plugins)
-
-services.forEach(name => {
-  if (!plugins[name]) { throw new Error(`Cant find plugin: ${name}`) }
-  seneca.use(plugins[name], config[name])
-})
-
-seneca.logger.info(`Services started: ${services.join(', ')}`)
-seneca.listen(config.listen)
+// const config = require('config')
+// const seneca = require('../src')
+const { requireDirectory, config } = require('../src/utils')
+console.log(config())
+//
+// const plugins = requireDirectory(`${process.cwd()}/src`)
+// const services = process.argv.length > 2 ? process.argv.slice(2) : Object.keys(plugins)
+//
+// services.forEach(name => {
+//   if (!plugins[name]) { throw new Error(`Cant find plugin: ${name}`) }
+//   seneca.use(plugins[name], config[name])
+// })
+//
+// seneca.logger.info(`Services started: ${services.join(', ')}`)
+// seneca.listen(config.listen)
